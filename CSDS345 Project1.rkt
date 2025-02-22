eee8b9487f9d838b6a8a4242eebf6275cec0c8f0
Ethan Tobey <etobey04@gmail.com>
2025-02-22 17:31:06 -0500

    some outline

--- CSDS345 Project1.rkt
+++ CSDS345 Project1.rkt
@@ -3,4 +3,34 @@
 (require "lex.rkt")
 
 (define interpret
-  (lambda (text)
+  (lambda (filename)
+    (parser filename)))
+
+
+;--------------statements-------------------------
+
+;declaration statement
+
+;assignment statement
+
+;return statement
+
+;if statement
+
+;while statement
+
+
+;----------------bindings -------------
+
+;create binding
+
+;lookup binding
+
+;update binding
+
+
+;---------------values-------------
+
+;Minteger
+
+;Mboolean
