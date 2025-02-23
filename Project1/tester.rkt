d6fdb51c2b7f9371719768991d1ff9005e51d526
joyoo9442 <joyoo9442@gmail.com>
2025-02-23 01:53:36 -0500

    added the boolean operators

--- Project1/tester.rkt
+++ Project1/tester.rkt
@@ -5,7 +5,7 @@
 (define ubd_error "ERROR (Usage before declaring)")
 (define uba_error "ERROR (Usage before assignment)")
 (define ubr_error "ERROR (redeclaration)")
-(define result_list (list 150 -4 10 16 220 5 6 10 5 -39 ubd_error ubd_error uba_error ubr_error true 100 false true 128 12 30 11 1106 12 16 72 21 164))
+(define result_list (list 150 -4 10 16 220 5 6 10 5 -39 ubd_error ubd_error uba_error ubr_error 'true 100 'false 'true 128 12 30 11 1106 12 16 72 21 164))
 
 (define dotest #t)
 (define testnumstring "")
